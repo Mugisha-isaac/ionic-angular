1e755b28a53560e8150f83ad327438af5e38600a
Mugisha-isaac <mugishaisaac2001example.com>
2021-05-02 21:40:39 +0200

    created recipe service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipesService} from './recipes.service';
 import {Recipe} from './recipe.model';
 
 @Component({
@@ -7,24 +8,11 @@ import {Recipe} from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
-  recipes: Recipe[] = [
-    {
-      id:'r1',
-      title:'Schnitzer',
-      imageUrl:'https://media-cdn.tripadvisor.com/media/photo-s/10/b7/14/b4/chicken-schnitzer-with.jpg',
-      ingredients:['Frensh Fries','Pork meat','Salad']
-    },
-    {
-      id:'r2',
-      title:'Spaghetti',
-      imageUrl:'https://img.taste.com.au/kmT_aJW8/taste/2016/11/spaghetti-with-tuna-sauce-13068-1.jpeg',
-      ingredients:['spaghettin','meat','tomatoes'],
-    }
-  ]
-  constructor() { }
+  recipes: Recipe[];
+  constructor(private recipeService: RecipesService) { }
 
   ngOnInit() {
+  this.recipes=this.recipeService.getAllRecipes();
   }
 
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+ private recipes: Recipe[] = [
+    {
+      id:'r1',
+      title:'Schnitzer',
+      imageUrl:'https://media-cdn.tripadvisor.com/media/photo-s/10/b7/14/b4/chicken-schnitzer-with.jpg',
+      ingredients:['Frensh Fries','Pork meat','Salad']
+    },
+    {
+      id:'r2',
+      title:'Spaghetti',
+      imageUrl:'https://img.taste.com.au/kmT_aJW8/taste/2016/11/spaghetti-with-tuna-sauce-13068-1.jpeg',
+      ingredients:['spaghettin','meat','tomatoes'],
+    }
+  ]
+  constructor() { }
+
+  getAllRecipes(){
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId:string){
+   return {...this.recipes.find(recipe=>{
+     return recipe.id===recipeId;
+   })};
+  }
+}
