1e0b5a314d5ab1ea17157ace145b20da4a3ff070
Mugisha-isaac <mugishaisaac2001example.com>
2021-05-02 21:56:11 +0200

    added param routing

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import {RecipesService} from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.page.scss'],
 })
 export class RecipeDetailPage implements OnInit {
-
-  constructor() { }
+ loadedRecipe:Recipe;
+  constructor(private acivatedRoute:ActivatedRoute, private recipesService:RecipesService) { }
 
   ngOnInit() {
+    this.acivatedRoute.paramMap.subscribe(paraMap=>{
+     if(!paraMap.has('recipeId')){
+       // redirect the user
+       return;
+     }
+     const recipeId = paraMap.get('recipeId');
+     this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    })
   }
 
 }
