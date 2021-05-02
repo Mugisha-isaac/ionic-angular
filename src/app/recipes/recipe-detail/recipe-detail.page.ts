da96f241eff873834d2757f71ae82d69e1cd09bd
Mugisha-isaac <mugishaisaac2001example.com>
2021-05-02 23:05:19 +0200

    added an alert controller before deleting

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import {RecipesService} from '../recipes.service';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,7 +11,8 @@ import {RecipesService} from '../recipes.service';
 })
 export class RecipeDetailPage implements OnInit {
  loadedRecipe:Recipe;
-  constructor(private acivatedRoute:ActivatedRoute, private recipesService:RecipesService, private router:Router) { }
+  constructor(private acivatedRoute:ActivatedRoute, private recipesService:RecipesService, private router:Router,
+    private alertCtrl:AlertController) { }
 
   ngOnInit() {
     this.acivatedRoute.paramMap.subscribe(paraMap=>{
@@ -23,8 +25,22 @@ export class RecipeDetailPage implements OnInit {
     })
   }
   onDeleteRecipe(){
+    this.alertCtrl.create({
+     header:'Are you sure?',
+     message:'Do you really want to delete this recipe?',
+     buttons:[{
+      text:'Cancel',
+      role:'cancel'
+     },
+    {
+     text:'Delete',
+     handler:()=>{
     this.recipesService.deleteRecipe(this.loadedRecipe.id);
     this.router.navigate(['/recipes']);
+     }
+    }
+    ]
+    }).then(alerEl=>alerEl.present());
   }
 
 }
